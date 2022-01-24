11b4661964b8c4948d28c129451187c8c052498b
knguyen2133 <knguyen2133@gmail.com>
2022-01-24 00:21:45 -0600

    fix syntax

--- misc/Rotated_String.py
+++ misc/Rotated_String.py
@@ -3,11 +3,12 @@
 
 def check_strings(str_a: str, str_b: str) -> bool:
     while i < len(str_b)-1:
-        if ( str_b[i] == str_a [0] )
-            for( j in str_a):
-                if (str_b[i % len(str_b)-1] != j):
+        if ( str_b[i] == str_a [0] ):
+            for j in range(len(str_a)-1):
+                if (str_b[i % len(str_b)-1] != str_a[j]):
                     print("almost the same word")
                     return false;
+                i+=1;
             return true;
     print("two completely different words ")
     return false;
