587029f628bd34b7fda200a2901f30b0799afb7c
knguyen2133 <knguyen2133@gmail.com>
2022-02-05 17:20:57 -0600

    added comments

--- leetcode/OneEditDistance.cpp
+++ leetcode/OneEditDistance.cpp
@@ -1,3 +1,16 @@
+// Given two strings s and t, return true if they are both one edit distance apart, otherwise return false.
+
+// A string s is said to be one distance apart from a string t if you can:
+
+// Insert exactly one character into s to get t.
+// Delete exactly one character from s to get t.
+// Replace exactly one character of s with a different character to get t.
+
+// Constraints:
+
+// 0 <= s.length, t.length <= 104
+// s and t consist of lowercase letters, uppercase letters, and digits.
+
 class Solution {
 public:
     bool isOneEditDistance(string s, string t) {
